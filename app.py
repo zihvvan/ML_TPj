d535a6ab2fcba196c62e312c0a38c0640ff2f3c9
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-23 09:40:13 +0900

    requirements

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 import streamlit as st
-
-st.write("test")
+import seaborn as sns
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import pandas as pd
+df = pd.read_csv('/content/test_scores.csv')
