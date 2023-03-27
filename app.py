281a667063a7306a7ac551c358759c9b0f67a5bb
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-28 01:38:05 +0900

    ㅇ

--- app.py
+++ app.py
@@ -53,7 +53,6 @@ def split_dataset(pre_processed_df):
     return X_train, X_test, y_train, y_test
 
 def run_model(X_train, X_test, y_train, y_test):
-    import plotly.express as px
     # 모델 선언(선형회귀)
     model = LinearRegression()
     model.fit(X_train, y_train) # 훈련 세트로 학습
@@ -62,20 +61,8 @@ def run_model(X_train, X_test, y_train, y_test):
     y_pred = model.predict(X_test)
 
     # 관계도
+    st.write(f"coef_ {model.coef_}")
     st.write(f"intercept_ {model.intercept_}")
-
-
-    fig = px.scatter(model.coef_)
-    fig.update_layout(title='다항 회귀 계수',xaxis_nticks=36)
-    fig.layout.update({'width':800, 'height':600})
-    st.plotly_chart(fig)
-
-
-    fig = px.imshow(concated_df.corr(),text_auto=True, color_continuous_scale='RdBu_r', aspect='auto')
-    fig.update_layout(title='컬럼별 상관관계',xaxis_nticks=36)
-    fig.layout.update({'width':800, 'height':600})
-    st.plotly_chart(fig)
-
 
 
     # 성능평가
