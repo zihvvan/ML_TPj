91d893313981d819a264d86e8ae3f72f1cef0665
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-29 06:57:15 +0900

    s

--- data_preprocess.py
+++ data_preprocess.py
@@ -5,6 +5,7 @@ import joblib
 import plotly.express as px
 from PIL import Image
 from math import sqrt
+from sklearn.compose import ColumnTransformer
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import r2_score
