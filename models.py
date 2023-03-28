88ea43ad75e1915559be4a0a5cd4a410938c789a
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-28 15:38:39 +0900

    ㅇㅇ

--- models.py
+++ models.py
@@ -83,7 +83,7 @@ def lightGBM_model(df):
 
 def xgBoost_model(df):
     with st.echo(code_location="below"):
-        model_path = "Data/pkl/XGBoost_model.pkl"
+        model_path = "XGBoost_model.pkl"
         xgb_model = joblib.load(model_path)
         st.write("## XGBoost_model model")
 
