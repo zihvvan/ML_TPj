59b9a2e6c2ff5fabf2b3b6b0573b29bde2ad906d
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-29 03:07:26 +0900

    ck

--- pre_process.py
+++ pre_process.py
@@ -1,17 +1,6 @@
 import streamlit as st
-import seaborn as sns
 import pandas as pd
 import joblib
-import plotly.express as px
-from PIL import Image
-from math import sqrt
-from sklearn.metrics import mean_absolute_error
-from sklearn import preprocessing
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Lasso, Ridge
-from sklearn.model_selection import GridSearchCV
 
 def pre_processing(df):
     # 필요없는 Columns 드랍
