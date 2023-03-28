042d2b86c86c097ad69fa7954d436be0b71ee365
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-29 05:50:23 +0900

    ㄴ

--- linear_process.py
+++ linear_process.py
@@ -108,6 +108,11 @@ def linear_processed_df(df, s_df, comparison1):
                                marker=dict(color=colors, size=8))
     fig.add_trace(scatter_trace, row=1, col=1)
     scatter_trace1 = go.Scatter(x=comparison1["실제값"], y=comparison1["예측값"], mode="markers", 
-                               marker=dict(color=colors, size=8))
+                                marker=dict(
+            color=x,  # set color equal to a variable
+            colorscale="Viridis",  # choose a colorscale
+            showscale=True,  # show the color scale
+            colorbar=dict(title="Colorbar"),  # set the title of the colorbar
+        ),)
     fig.add_trace(scatter_trace1, row=1, col=2)
     st.plotly_chart(fig)
