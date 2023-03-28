5d834c8896f18144391f99973e74e0950c033d8a
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-28 18:29:51 +0900

    age

--- models.py
+++ models.py
@@ -54,7 +54,7 @@ def random_forest_model(df):
     st.header("random_forest")
     # 첫번째 행
     r1_col1, r1_col2, r1_col3, r1_col4  = st.columns(4)
-    나이 = r1_col1.slider("나이",1,80)
+    나이 = r1_col1.slider("나이",20,70)
     일일급여 = r1_col2.slider("일일급여", 1, 1500)
     회사와의거리 = r1_col3.slider("회사와의거리", 1, 30)
     근무환경만족 = r1_col4.slider("근무환경만족", 1, 4)
