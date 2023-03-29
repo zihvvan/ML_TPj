9f85a236ab1123b8840fd1149f417075727676b7
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-29 15:52:57 +0900

    days

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def cal_time():
     future_date = date(2023, 8, 24)
     delta = future_date - today
 
-    return delta
+    return delta.days
 
 def main():
     image1 = Image.open('image/main.webp')
@@ -19,10 +19,11 @@ def main():
             ("성적 예측 모델", "회사퇴사 예측 모델")
         )
         days = cal_time()
-        
+        <div align=center><h1>📚 STACKS</h1></div>
         st.markdown(f"<div style='font-weight:bold; font-size:40px; text-align:center'>D - {days}</div>", unsafe_allow_html=True)
         st.markdown(f"<div style='font-weight:bold; font-size:40px; text-align:center'>Quit? or Not!</div>", unsafe_allow_html=True)
         st.markdown("---")
+        st.markdown(f"<div align=center><h1>📚 STACKS</h1></div>", unsafe_allow_html=True)
         st.markdown("[![Git](https://img.shields.io/badge/git-444444?style=for-the-badge&logo=git)](https://github.com/mastgm0817/ML_TPrj)")
         st.markdown("[![Notion](https://img.shields.io/badge/Notion-444444?style=for-the-badge&logo=Notion)](https://bit.ly/3lSMdPR)")
     if add_selectbox == "성적 예측 모델":
