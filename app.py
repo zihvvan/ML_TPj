3a87a75a48bc8392c1ef03cd9f73f031152a2f14
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-29 15:27:05 +0900

    notion

--- app.py
+++ app.py
@@ -21,9 +21,10 @@ def main():
             ("성적 예측 모델", "회사퇴사 예측 모델")
         )
         days = cal_time()
-
+        
         st.markdown(f"<div style='font-weight:bold; font-size:40px; text-align:center'>D - {days}</div>", unsafe_allow_html=True)
         st.markdown(f"<div style='font-weight:bold; font-size:40px; text-align:center'>Quit? or Not!</div>", unsafe_allow_html=True)
+        st.markdown("---")
         st.markdown("![Git](https://img.shields.io/badge/git-444444?style=for-the-badge&logo=git)[Git](https://github.com/mastgm0817/ML_TPrj)")
         st.markdown("![Notion](https://img.shields.io/badge/Notion-444444?style=for-the-badge&logo=Notion)[Notion](http://bit.ly/3lSMdPR)")
 
