09935ac030089dc6aa6a36e11581ec1789efb712
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-03-29 14:18:56 +0900

    수정 진행중

--- view.py
+++ view.py
@@ -14,18 +14,25 @@ def view_model1():
     with tab2:
             st.header("원본 데이터")
             st.write(df)
+            st.write("---")
             st.header("데이터셋 통계자료 ")
-            processed_df = pre_process.s_pre_processing(df)
-            visualization_process_score.describe_linear_model(df)
+            st.write("데이터셋 전체에 대한 통계 정보를 확인합니다.")
+            processed_df = pre_process.pre_processing(df)
+            visualization_process.describe_linear_model(df)
+            st.write("---")
             st.header("데이터셋 Drop & One-Hot Enconding")
+            st.write("불필요한 열 제거, 결측지 처리, 범주형 변수 처리등의 이유로 데이터 전처리 과정에서")
+            st.write("분석의 효율성을 높이기 위해 필요한 과정입니다.")
             st.write(processed_df)
+            st.write("---")
             st.header("Min-Max Scaling")
-            data_frame2 = data_preprocess_score.linear_process(df)
-            s_df, comparison, data_frame1 = data_preprocess_score.poly_model(df)
-            data_preprocess_score.draw_table(data_frame1, data_frame2 )
+            st.write("데이터 정규화, 이상치 처리, 머신 러닝 모델 성능 향상을 위한 데이터 전처리 기법")
+            data_frame2 = data_preprocess.linear_process(df)
+            s_df, comparison, data_frame1 = data_preprocess.poly_model(df)
+            data_preprocess.draw_table(data_frame1, data_frame2 )
     with tab3:
             st.header("시각화")
-            visualization_process_score.visualization(df, s_df, comparison)
+            visualization_process.visualization(df, s_df, comparison
 
 
 
