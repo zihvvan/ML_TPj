78d942f587736d2d53ce2d8747bbde90fcb37f83
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-29 15:31:13 +0900

    final

--- app.py
+++ app.py
@@ -25,9 +25,8 @@ def main():
         st.markdown(f"<div style='font-weight:bold; font-size:40px; text-align:center'>D - {days}</div>", unsafe_allow_html=True)
         st.markdown(f"<div style='font-weight:bold; font-size:40px; text-align:center'>Quit? or Not!</div>", unsafe_allow_html=True)
         st.markdown("---")
-        st.markdown("![Git](https://img.shields.io/badge/git-444444?style=for-the-badge&logo=git)[Git](https://github.com/mastgm0817/ML_TPrj)")
-        st.markdown("![Notion](https://img.shields.io/badge/Notion-444444?style=for-the-badge&logo=Notion)[Notion](http://bit.ly/3lSMdPR)")
-
+        st.markdown("[![Git](https://img.shields.io/badge/git-444444?style=for-the-badge&logo=git)](https://github.com/mastgm0817/ML_TPrj)")
+        st.markdown("[![Notion](https://img.shields.io/badge/Notion-444444?style=for-the-badge&logo=Notion)](http://bit.ly/3lSMdPR)")
     if add_selectbox == "성적 예측 모델":
         view.view_model1()
     else:
