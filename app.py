24e2747d5b7fcc1e80b5b5233a47e32ee6ed4702
고병연 <59509808+mastgm0817@users.noreply.github.com>
2023-03-29 15:14:40 +0900

    s

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ def main():
             ("성적 예측 모델", "회사퇴사 예측 모델")
         )
         days = cal_time()
-        st.header(f"D - {days}")
+        # st.header(f"D - {days}")
+        st.markdown(f"<div style='font-weight:bold; font-size:40px; text-align:center'>D - {days}</div>", unsafe_allow_html=True)
         st.markdown(f"<div style='font-weight:bold; font-size:40px; text-align:center'>Quit? or Not!</div>", unsafe_allow_html=True)
 
     if add_selectbox == "성적 예측 모델":
